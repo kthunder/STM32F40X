c86e3a36638c8aa989c124cada838967edc1c479
konglei <konglei@thinktech.net.cn>
2023-06-27 09:50:08 +0800

    update gpio typedef

--- src/Core/Src/main.c
+++ src/Core/Src/main.c
@@ -12,72 +12,46 @@
 
 void SystemInit()
 {
-	RCC_Init();
+    RCC_Init();
 }
 
 void GpioInit()
 {
     GPIO_InitTypeDef config = {0};
-    config.Pin = GPIO_Pin_6;
-    config.Speed = GPIO_SPEED_FREQ_HIGH;
-    config.Mode = GPIO_MODE_OUTPUT_PP;
-    config.Pull = 0;
-    config.Alternate = ;
 
-	// GPIO_InitTypeDef ucGpioConfig[] = {
-	// 	{
-	// 		.GPIO_Pin = GPIO_Pin_9,
-	// 		.GPIO_Mode = GPIO_Mode_AF_PP,
-	// 		.GPIO_Speed = GPIO_Speed_50MHz,
-	// 	},
-	// 	{
-	// 		.GPIO_Pin = GPIO_Pin_8,
-	// 		.GPIO_Mode = GPIO_Mode_AF_PP,
-	// 		.GPIO_Speed = GPIO_Speed_50MHz,
-	// 	},
-	// 	{
-	// 		.GPIO_Pin = GPIO_Pin_13,
-	// 		.GPIO_Mode = GPIO_Mode_OUT_PP,
-	// 		.GPIO_Speed = GPIO_Speed_50MHz,
-	// 	},
-	// };
+    GPIOA->MODER |= GPIO_MODER_MODER6_0;
+    GPIOA->MODER |= GPIO_MODER_MODER7_0;
+    GPIOA->ODR = 0x0;
 
-	// GPIO_Init(GPIOA, &ucGpioConfig[0]);
-	// GPIO_Init(GPIOA, &ucGpioConfig[1]);
-	// GPIO_Init(GPIOC, &ucGpioConfig[2]);
-	GPIOA->MODER |= GPIO_MODER_MODER6_0;
-	GPIOA->MODER |= GPIO_MODER_MODER7_0;
-	GPIOA->ODR = 0x0;
-
-	GPIOA->MODER |= GPIO_MODER_MODER9_1;
-	GPIOA->AFR[1] != GPIO_AFRH_AFRH1_0;
-	GPIOA->AFR[1] != GPIO_AFRH_AFRH1_1;
-	GPIOA->AFR[1] != GPIO_AFRH_AFRH1_2;
+    GPIOA->MODER |= GPIO_MODER_MODER9_1;
+    GPIOA->AFR[1] != GPIO_AFRH_AFRH1_0;
+    GPIOA->AFR[1] != GPIO_AFRH_AFRH1_1;
+    GPIOA->AFR[1] != GPIO_AFRH_AFRH1_2;
 }
 
 void task_heart_beat(void *pvParameters)
 {
-	GPIO_TogglePin(GPIOA, GPIO_Pin_6);
-	while (1)
-	{
-		log_info("heart beat %d ms", HAL_GetTick());
-		GPIO_TogglePin(GPIOA, GPIO_Pin_6);
-		GPIO_TogglePin(GPIOA, GPIO_Pin_7);
-		delay_ms(1000);
-	}
+    GPIO_TogglePin(GPIOA, GPIO_Pin_6);
+    while (1)
+    {
+        log_info("heart beat %d ms", HAL_GetTick());
+        GPIO_TogglePin(GPIOA, GPIO_Pin_6);
+        GPIO_TogglePin(GPIOA, GPIO_Pin_7);
+        delay_ms(1000);
+    }
 }
 
 uint32_t testSVC(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3)
 {
-	__NOP();
-	__ASM __volatile("svc #1\n");
-	__NOP();
-	return 0;
+    __NOP();
+    __ASM __volatile("svc #1\n");
+    __NOP();
+    return 0;
 }
 
 int main()
 {
-	GpioInit();
-	USART_Init(USART1);
-	task_heart_beat(NULL);
+    GpioInit();
+    USART_Init(USART1);
+    task_heart_beat(NULL);
 }
