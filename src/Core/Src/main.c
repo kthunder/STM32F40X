dcb97b8ea0d3db82dbf3207b4b3cd0f2159deba5
konglei <konglei@thinktech.net.cn>
2023-09-26 17:21:05 +0800

    update

--- src/Core/Src/main.c
+++ src/Core/Src/main.c
@@ -4,55 +4,65 @@
 #include "log.h"
 #include "tools.h"
 
-#include "stm32f4xx.h"
 #include "hal_flash.h"
 #include "hal_gpio.h"
 #include "hal_rcc.h"
-#include "hal_usart.h"
 #include "hal_spi.h"
+#include "hal_usart.h"
+#include "stm32f4xx.h"
+
 
 #include "MinRTOS.h"
 
 /* private func*/
-void LedInit()
-{
-    GPIO_InitTypeDef led_config = {
-        .pin = GPIO_Pin_6 | GPIO_Pin_7,
-        .mode = GPIO_MODE_OUT,
-        .otype = GPIO_OTYPE_PP,
-        .speed = GPIO_SPEED_FREQ_HIGH,
-        .pupd = GPIO_PUPD_NONE,
-        .af = GPIO_AF_0,
-    };
-    GPIO_Init(GPIOA, &led_config);
+void LedInit() {
+  GPIO_InitTypeDef led_config = {
+      .pin = GPIO_Pin_13,
+      .mode = GPIO_MODE_OUT,
+      .otype = GPIO_OTYPE_PP,
+      .speed = GPIO_SPEED_FREQ_HIGH,
+      .pupd = GPIO_PUPD_NONE,
+      .af = GPIO_AF_0,
+  };
+  GPIO_Init(GPIOC, &led_config);
 }
-void UartInit()
-{
-    GPIO_InitTypeDef usart1_config = {
-        .pin = GPIO_Pin_9,
-        .mode = GPIO_MODE_AF_OUT,
-        .otype = GPIO_OTYPE_PP,
-        .speed = GPIO_SPEED_FREQ_HIGH,
-        .pupd = GPIO_PUPD_PULL_UP,
-        .af = GPIO_AF_7,
-    };
-    GPIO_Init(GPIOA, &usart1_config);
-    USART_Init(USART1);
+void McoInit() {
+  GPIO_InitTypeDef config = {
+      .pin = GPIO_Pin_8,
+      .mode = GPIO_MODE_AF_OUT,
+      .otype = GPIO_OTYPE_PP,
+      .speed = GPIO_SPEED_FREQ_HIGH,
+      .pupd = GPIO_PUPD_NONE,
+      .af = GPIO_AF_0,
+  };
+  GPIO_Init(GPIOA, &config);
+}
+void UartInit() {
+  GPIO_InitTypeDef usart1_config = {
+      .pin = GPIO_Pin_9,
+      .mode = GPIO_MODE_AF_OUT,
+      .otype = GPIO_OTYPE_PP,
+      .speed = GPIO_SPEED_FREQ_HIGH,
+      .pupd = GPIO_PUPD_PULL_UP,
+      .af = GPIO_AF_7,
+  };
+  GPIO_Init(GPIOA, &usart1_config);
+  USART_Init(USART1);
 }
 
-int main()
-{
-    // LedInit();
-    UartInit();
+int main() {
+  LedInit();
+  McoInit();
+  UartInit();
 
-    // test_spi();
+  // test_spi();
 
-    while (1)
-    {
-        delay_ms(1000);
-        GPIO_TogglePin(GPIOA, GPIO_Pin_7 | GPIO_Pin_6);
-        log_info("heart beat!\n");
-    }
+  while (1) {
+    
+    delay_ms(1000);
+    GPIO_TogglePin(GPIOC, GPIO_Pin_13);
+    log_info("heart beat!\n");
+  }
 
-    // vTaskStartScheduler();
+  // vTaskStartScheduler();
 }
